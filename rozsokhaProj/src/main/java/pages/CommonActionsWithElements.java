906868a19955ef2edf74e3d87093d658647a8e62
Max Rozsokha <m.rozsokha@spd-ukraine.com>
2025-06-19 20:58:52 +0300

    auto test LoginTestWithPageObject.java part 1

---
+++ rozsokhaProj/src/main/java/pages/CommonActionsWithElements.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonActionsWithElements {
+    protected WebDriver webDriver;
+
+    public CommonActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+}
