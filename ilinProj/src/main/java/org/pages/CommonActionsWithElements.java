7aa4354ee1ef3fcdc1d4d877dcad286ee2315d77
artemilin <ilinartem293@gmail.com>
2025-06-19 20:59:01 +0300

    auto test LoginTestWithPageObject.jave part 1

---
+++ ilinProj/src/main/java/org/pages/CommonActionsWithElements.java
@@ -0,0 +1,12 @@
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CommonActionsWithElements {
+    protected WebDriver webDriver;
+
+    public CommonActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+}
