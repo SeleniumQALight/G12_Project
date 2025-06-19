f73ea79f8c45a7f4957912ce46c50d18372db9f7
artemilin <ilinartem293@gmail.com>
2025-06-19 21:37:28 +0300

    auto test LoginTestWithPageObject.jave part 2

--- ilinProj/src/main/java/org/pages/CommonActionsWithElements.java
+++ ilinProj/src/main/java/org/pages/CommonActionsWithElements.java
@@ -2,11 +2,13 @@ package org.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
 
     public CommonActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);// ініціалізує елементи описані в FindBy; в this прийдуть усі елементи з яких ми прийдепо (LoginPage, HomePage)
     }
 }
