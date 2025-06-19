b4ab738aa33050f6e957ef649edafb6f0c0889d7
alina <alinadorosh65@gmail.com>
2025-06-19 19:38:01 +0300

    first auto test was fixed

--- DoroshProj/src/test/java/org/loginTests/LoginTestAllStepsInOneClass.java
+++ DoroshProj/src/test/java/org/loginTests/LoginTestAllStepsInOneClass.java
@@ -56,9 +56,14 @@ public class LoginTestAllStepsInOneClass {
     }
 
     private boolean isButtonSignOutVisible() {
-        boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-        logger.info(" element visible - " + state);
-        return state;
+        try {
+            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+            logger.info(" element visible - " + state);
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is not found");
+            return false;
+        }
     }
 
 }
