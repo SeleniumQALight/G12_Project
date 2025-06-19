a503df595ab799a4ada42024f0500be55d34de14
tradulenko <radulenko@gmail.com>
2025-06-19 20:58:49 +0300

    auto test LoginTestWithPageObject.java part 1

---
+++ overAll/src/main/java/org/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package org.pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends ParentPage{
+    private Logger logger = Logger.getLogger(getClass());
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void openLoginPage() {
+        webDriver.get(baseURL);
+        logger.info("Login page was opened with url " + baseURL);
+    }
+
+    public void enterTextIntoInputLogin(String login) {
+        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
+        inputUserName.clear();
+        inputUserName.sendKeys(login);
+        logger.info(login + " was entered in input UserName");
+    }
+
+    public void enterTextIntoPassword(String password){
+        WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys(password);
+        logger.info(password + " was entered in input Password");
+    }
+
+    public void clickOnButtonSignIn(){
+        webDriver.findElement(By.xpath("//button[text()='Sign In']")).click();
+        logger.info("Button Sign In was clicked");
+    }
+
+}
