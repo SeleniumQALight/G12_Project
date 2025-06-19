d2bfd6df3d21f75ac0899b19d954f076cba3ee40
Andrew <my_email@test.com>
2025-06-19 21:37:16 +0300

    auto test LoginTestWithPageObject.java part2 FindBy

--- dylProject/src/main/java/org/pages/CommonActionsWithElements.java
+++ dylProject/src/main/java/org/pages/CommonActionsWithElements.java
@@ -1,12 +1,14 @@
 package org.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
 
     public CommonActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this); // ініціалізує елементи описані в FindBy
     }
 
 }
