7c9f4325141d785084ad72ff0780582876e372d2
alina <alinadorosh65@gmail.com>
2025-06-19 20:58:52 +0300

    auto test LoginTestWithPageObject.java part 1

---
+++ DoroshProj/src/main/java/org/pages/CommonActionsWithElements.java
@@ -0,0 +1,13 @@
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonActionsWithElements {
+    protected WebDriver webDriver;
+
+    public CommonActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+
+}
