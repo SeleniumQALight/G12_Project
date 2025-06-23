0d716e50ec907a6020c29f6755ecdfea1b8f853f
POlesya <olesya.pilip@gmail.com>
2025-06-23 11:29:18 +0300

    Added test for invalid login - fixed

--- pylypProject/src/test/java/org/loginTests/LoginTestAllStepsInOneClass.java
+++ pylypProject/src/test/java/org/loginTests/LoginTestAllStepsInOneClass.java
@@ -18,40 +18,43 @@ public class LoginTestAllStepsInOneClass {
     private WebDriver webDriver;
     private Logger logger = Logger.getLogger(getClass());
 
-@Before
-    public void setUp(){
-    WebDriverManager.chromedriver().setup();
-    webDriver = new ChromeDriver();
-    webDriver.manage().window().maximize();
-    webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    logger.info("Browser was opened");
-}
-@After
-    public void tearDown(){
-    webDriver.quit();
-    logger.info("Browser was closed");
-}
-@Test
-    public void validLogin(){
-    webDriver.get("https://aqa-complexapp.onrender.com/");
-    logger.info("Site was opened");
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("Browser was opened");
+    }
 
-    WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
-    inputUserName.clear();
-    inputUserName.sendKeys("qaauto");
-    logger.info("qaauto was entered in input UserName");
+    @After
+    public void tearDown() {
+        webDriver.quit();
+        logger.info("Browser was closed");
+    }
 
-    WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
-    inputPassword.clear();
-    inputPassword.sendKeys("123456qwerty");
-    logger.info("password was entered in input Password");
+    @Test
+    public void validLogin() {
+        webDriver.get("https://aqa-complexapp.onrender.com/");
+        logger.info("Site was opened");
 
-    webDriver.findElement(By.xpath("//button[text()='Sign In']")).click();
-    logger.info("Button Sign In was clicked");
+        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
+        inputUserName.clear();
+        inputUserName.sendKeys("qaauto");
+        logger.info("qaauto was entered in input UserName");
 
-    Assert.assertTrue("User is not LoggedIn: button SignOut is not visible" , isButtonSignOutVisible() );
+        WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwerty");
+        logger.info("password was entered in input Password");
 
-}
+        webDriver.findElement(By.xpath("//button[text()='Sign In']")).click();
+        logger.info("Button Sign In was clicked");
+
+        Assert.assertTrue("User is not LoggedIn: button SignOut is not visible", isButtonSignOutVisible());
+
+    }
+
     @Test
     public void invalidLogin() {
         webDriver.get("https://aqa-complexapp.onrender.com/");
@@ -82,17 +85,24 @@ public class LoginTestAllStepsInOneClass {
         Assert.assertTrue("Error message is not displayed", alertMessage.isDisplayed());
         logger.info("Error message is visible: " + alertMessage.getText());
     }
-
-
     private boolean isButtonSignOutVisible() {
-    try {
-    boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-    logger.info( " element visible - " + state);
-    return state;
-    }catch (Exception e){
-    logger.info("Element is not found");
-    return false;
+        try {
+            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+            logger.info("Element visible - " + state);
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is not found");
+            return false;
+        }
     }
+    private boolean isButtonSignInVisible() {
+        try {
+            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign In']")).isDisplayed();
+            logger.info("Sign In button visible - " + state);
+            return state;
+        } catch (Exception e) {
+            logger.info("Sign In button is not found");
+            return false;
+        }
     }
-
 }
