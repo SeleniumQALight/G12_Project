0783140c90b36d3ad95c3cc2c55ce96ac3a6798c
Max Rozsokha <m.rozsokha@spd-ukraine.com>
2025-06-24 21:42:20 +0300

    Methods were added to CommonActionsWithElements

--- rozsokhaProj/src/main/java/pages/HomePage.java
+++ rozsokhaProj/src/main/java/pages/HomePage.java
@@ -1,29 +1,33 @@
 package pages;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage {
-    Logger logger = Logger.getLogger(getClass());
+//    Logger logger = Logger.getLogger(getClass());
+
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement buttonSignOut;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void checkButtonSignOutVisible() {
-        Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
+//        Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
+        checkIsElementDisplayed(buttonSignOut);
     }
 
-    private boolean isButtonSignOutVisible() {
-        try {
-            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-            logger.info("is element visible - " + state);
-            return state;
-        } catch (Exception e) {
-            logger.info("Element SignOut is not visible");
-            return false;
-        }
-    }
+//    private boolean isButtonSignOutVisible() {
+//        try {
+//            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+//            logger.info("is element visible - " + state);
+//            return state;
+//        } catch (Exception e) {
+//            logger.info("Element SignOut is not visible");
+//            return false;
+//        }
+//    }
 }
--- rozsokhaProj/src/main/java/pages/LoginPage.java
+++ rozsokhaProj/src/main/java/pages/LoginPage.java
@@ -30,22 +30,25 @@ public class LoginPage extends ParentPage {
 
     public void enterTextIntoInputLogin(String login) {
 //        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
-        inputUserName.clear();
-        inputUserName.sendKeys(login);
-        logger.info(login + " was set to input UserName");
+//        inputUserName.clear();
+//        inputUserName.sendKeys(login);
+//        logger.info(login + " was set to input UserName");
+        clearAndEnterTextToElement(inputUserName, login);
 
     }
 
     public void enterTextIntoPassword(String password) {
 //        WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
-        inputPassword.clear();
-        inputPassword.sendKeys(password);
-        logger.info(password + " password was set to input Password");
+//        inputPassword.clear();
+//        inputPassword.sendKeys(password);
+//        logger.info(password + " password was set to input Password");
+        clearAndEnterTextToElement(inputPassword, password);
     }
 
     public void clickOnButtonSignIn() {
 //        webDriver.findElement(By.xpath("//button[text()='Sign In']")).click();
-        buttonSignIn.click();
-        logger.info("Button Sign In was clicked");
+//        buttonSignIn.click();
+//        logger.info("Button Sign In was clicked");
+        clickOnElement(buttonSignIn);
     }
 }
