123e0af5aab6d6d29606695a462e62aaa742e579
artemilin <ilinartem293@gmail.com>
2025-06-24 21:42:24 +0300

    Methods were added to CommonActionsWithElements

--- ilinProj/src/main/java/org/pages/HomePage.java
+++ ilinProj/src/main/java/org/pages/HomePage.java
@@ -1,30 +1,23 @@
 package org.pages;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage {
-    Logger logger = Logger.getLogger(getClass());
+//    Logger logger = Logger.getLogger(getClass());
+
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement buttonSignOut;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void checkButtonSignOutVisible() {
-        Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
+        //Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
+        checkIsElementDisplayed(buttonSignOut);
     }
 
-    private boolean isButtonSignOutVisible() {
-        try {
-            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-            logger.info("element visible - " + state);
-            return state;
-        } catch (Exception e) {
-            logger.info("Element is not found");
-            return false;
-        }
-
-    }
 }
