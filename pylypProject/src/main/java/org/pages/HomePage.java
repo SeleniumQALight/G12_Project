316d76aca22b2670655edfdd5d6daf7f705846c8
dmytro.ned <dmytro.ned@brainstack.team>
2025-07-01 09:17:44 +0300

    fixed deleted imports in non nedbailoProj

--- pylypProject/src/main/java/org/pages/HomePage.java
+++ pylypProject/src/main/java/org/pages/HomePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import javax.net.ssl.HostnameVerifier;
+
 public class HomePage extends ParentPage{
 //    Logger logger = Logger.getLogger(getClass());
 
