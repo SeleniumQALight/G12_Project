aed622d05359ad02a1b89eac4b757037e2256ec2
anjelikataneeva <lika.taneeeva@gmail.com>
2025-07-03 01:57:06 +0300

    Created new post part 2

--- taneevaProj/src/test/java/org/postsTest/CreateNewPostTest.java
+++ taneevaProj/src/test/java/org/postsTest/CreateNewPostTest.java
@@ -1,18 +1,24 @@
 package org.postsTest;
 
 import org.baseTest.BaseTest;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.After;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+    //GUID =
+
+    final String POST_TITLE = "TR001_G12 Anj" + Utils_Custom.getDateAndTimeFormatted();
     @Test
-    public void createNewPost() {
+    public void TR001_createNewPost() {
         pageProvider.getLoginPage()
                 .openLoginPageAndFIllLoginFormWithValidCred()
                 .checkIsRedirectedToHomePage()
                 .getHeaderForLoggedUserElement().clickOnButtonCreatePost()
                 .checkIsRedirectedToCreateNewPostPage()
-                .enterTextIntoInputTitle("G12 Ador Post Title")
-                .enterTextIntoInputBody("Post Body created by Ador")
+                .enterTextIntoInputTitle(POST_TITLE)
+                .enterTextIntoInputBody("Body of the post created by Anj")
                 .clickOnButtonSaveNewPost()
                 .checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
@@ -20,6 +26,15 @@ public class CreateNewPostTest extends BaseTest {
 
 ;
         pageProvider.getPostPage()
-                .getheaderForLoggedUserElement().clickOnButtonMyProfile();
+                .getheaderForLoggedUserElement().clickOnButtonMyProfile()
+                .checkIsRedirectedToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE, 1);
     }
+
+    @After
+    public void deletePost() {
+//        pageProvider.getMyProfilePage()
+//                .getListOfPostsWithTitle(POST_TITLE)
+//                .forEach(post -> post.findElement(By.xpath(".//button[text()='Delete']")).click());
+//        Utils_Custom.waitABit(2);
 }
