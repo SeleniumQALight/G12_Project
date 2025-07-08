4c57d1a281f8f5005e8b391d3c0217ece6b2c934
tradulenko <radulenko@gmail.com>
2025-07-08 21:13:49 +0300

    CreateNewPostTest Part 3 Post condition

--- overAll/src/main/java/org/pages/HomePage.java
+++ overAll/src/main/java/org/pages/HomePage.java
@@ -1,12 +1,14 @@
 package org.pages;
 
 
+import org.apache.log4j.Logger;
+import org.data.TestData;
 import org.openqa.selenium.WebDriver;
 
 import org.pages.elements.HeaderForLoggedUserElement;
 
-public class HomePage extends ParentPage{
-
+public class HomePage extends ParentPage {
+    Logger logger = Logger.getLogger(getClass());
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
@@ -23,4 +25,20 @@ public class HomePage extends ParentPage{
         return this;
     }
 
+    public HomePage openHomePageAndLoginIfNeeded() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.openLoginPage();
+        if (getHeaderForLoggedUserElement().isButtonSignOutVisible()) {
+            logger.info("User is already logged in");
+        } else {
+            loginPage.enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
+
+
+                    .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
+                    .clickOnButtonSignIn();
+            checkIsRedirectToHomePage();
+            logger.info("User was logged in");
+        }
+        return this;
+    }
 }
--- overAll/src/main/java/org/pages/MyProfilePage.java
+++ overAll/src/main/java/org/pages/MyProfilePage.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
@@ -12,6 +13,9 @@ public class MyProfilePage extends ParentPage{
     Logger logger = Logger.getLogger(getClass());
 
     private String postWithTitleLocator = "//*[text()='%s']";
+
+    @FindBy(xpath = "//*[text()='Post successfully deleted.']")
+    private WebElement successMessageDelete;
 
     public MyProfilePage(WebDriver webDriver) {
         super(webDriver);
@@ -33,6 +37,32 @@ public class MyProfilePage extends ParentPage{
                 expectedAmountOfPosts,
                 getListOfPostsWithTitle(postTitle).size());
         logger.info("Post with title '" + postTitle + "' is present");
+        return this;
+    }
+
+    public MyProfilePage deletePostsTillPresent(String postTitle) {
+        List<WebElement> postsList = getListOfPostsWithTitle(postTitle);
+        final int MAX_POST_COUNT = 100; // postsList.size()
+        int counter = 0;
+        while (!postsList.isEmpty() && (counter < MAX_POST_COUNT)){
+            clickOnElement(postsList.get(0));
+            new PostPage(webDriver)
+                    .checkIsRedirectToPostPage()
+                    .clickOnDeleteButton()
+                    .checkIsRedirectToMyProfilePage()
+                    .checkIsMessageSuccessDeletePresent();
+            logger.info("Post with title " + postTitle + " was deleted");
+            postsList = getListOfPostsWithTitle(postTitle);
+            counter++; // counter = counter + 1;
+        }
+        if (counter >= MAX_POST_COUNT){
+            logger.error("Number of posts with title " + postTitle + " is more than " + MAX_POST_COUNT);
+        }
+        return this;
+    }
+
+    private MyProfilePage checkIsMessageSuccessDeletePresent() {
+        checkIsElementDisplayed(successMessageDelete);
         return this;
     }
 }
