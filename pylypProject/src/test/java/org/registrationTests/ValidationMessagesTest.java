180f959e11715fb07e301aaa49b9541d729ce490
POlesya <olesya.pilip@gmail.com>
2025-07-15 21:45:24 +0300

    ValidationMessagesTest was implemented

---
+++ pylypProject/src/test/java/org/registrationTests/ValidationMessagesTest.java
@@ -0,0 +1,23 @@
+package org.registrationTests;
+
+import org.baseTest.BaseTest;
+import org.junit.Test;
+
+import static org.data.RegistrationValidationMessages.*;
+
+public class ValidationMessagesTest extends BaseTest {
+    @Test
+    public void TC03_testValidationMessages() {
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage()
+                .enterTextIntoRegistrationUserNameField("tr")
+                .enterTextIntoRegistrationEmailField("tr")
+                .enterTextIntoRegistrationPasswordField("tr")
+                .checkErrorMessage(ERROR_USERNAME +
+                        SEMICOLON +
+                        ERROR_EMAIL +
+                        SEMICOLON +
+                        ERROR_PASSWORD)
+                ;
+    }
+}
