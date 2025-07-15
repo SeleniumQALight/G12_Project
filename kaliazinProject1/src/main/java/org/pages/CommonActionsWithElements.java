d5adfcea941036a01a1c88ab95553eca87038411
yannchik <yan.kalyazin1@gmail.com>
2025-07-15 21:45:34 +0300

    ValidationMessagesTest was implemented

--- kaliazinProject1/src/main/java/org/pages/CommonActionsWithElements.java
+++ kaliazinProject1/src/main/java/org/pages/CommonActionsWithElements.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class CommonActionsWithElements {
 
