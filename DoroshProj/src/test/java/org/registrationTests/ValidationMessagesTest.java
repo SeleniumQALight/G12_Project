1496010b768c7b01cc7869c379f3bded5e2930ef
alina <alinadorosh65@gmail.com>
2025-07-17 20:11:24 +0300

    Test with params

--- DoroshProj/src/test/java/org/registrationTests/ValidationMessagesTest.java
+++ DoroshProj/src/test/java/org/registrationTests/ValidationMessagesTest.java
@@ -1,25 +1,38 @@
 package org.registrationTests;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.data.RegistrationValidationMessages.*;
+
+@RunWith(JUnitParamsRunner.class)
+
 
 
 public class ValidationMessagesTest extends BaseTest {
     @Test
-    public void TC03_testValidationMessages() {
-            pageProvider.getLoginPage().openLoginPage();
-            pageProvider.getLoginPage()
-                    .enterTextIntoRegistrationUserNameField("tr")
-                    .enterTextIntoRegistrationEmailField("tr")
-                    .enterTextIntoRegistrationPasswordField("tr")
-                    .checkErrorsMesssages(ERROR_USERNAME+
-                            SEMICOLON +
-                            ERROR_EMAIL+
-                            SEMICOLON +
-                            ERROR_PASSWORD)
-            ;
+    @Parameters(method = "parametersForTestValidationMessages")
+
+    public void TC03_testValidationMessages(
+            String userName, String email, String password, String expectedMessages) {
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage()
+                .enterTextIntoRegistrationUserNameField(userName)
+                .enterTextIntoRegistrationEmailField(email)
+                .enterTextIntoRegistrationPasswordField(password)
+                .checkErrorsMesssages(expectedMessages)
+        ;
+
+    }
+
+    public Object[][] parametersForTestValidationMessages(){
+        return new Object[][] {
+                {"tr", "tr1", "tr2", ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"taras", "tr1", "tr2", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD}
+        };
 
     }
 
