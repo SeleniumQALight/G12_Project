756abf791f58270ed74620bb20b868deebcbc45a
Max Rozsokha <m.rozsokha@spd-ukraine.com>
2025-07-17 20:10:58 +0300

    Test with params

--- rozsokhaProj/src/test/java/registrationTest/ValidationMessagesTest.java
+++ rozsokhaProj/src/test/java/registrationTest/ValidationMessagesTest.java
@@ -1,25 +1,34 @@
 package registrationTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static data.RegistrationValidationMessages.*;
 
+@RunWith(JUnitParamsRunner.class) // to start test several times with different parameters
 public class ValidationMessagesTest extends BaseTest {
     @Test
-    public void TC03_testValidationMessages() {
+    @Parameters(method = "parametersForTestValidationMessages")
+    public void TC03_testValidationMessages(
+            String userName, String email, String password, String expectedMessages) {
         pageProvider.getLoginPage().openLoginPage();
         pageProvider.getLoginPage()
-                .enterTextIntoRegistrationUserNameField("tr")
-                .enterTextIntoRegistrationEmailField("tr")
-                .enterTextIntoRegistrationPasswordField("tr")
-                .checkErrorsMessages(ERROR_USERNAME +
-                        SEMICOLON +
-                        ERROR_EMAIL +
-                        SEMICOLON +
-                        ERROR_PASSWORD +
-                        SEMICOLON)
+                .enterTextIntoRegistrationUserNameField(userName)
+                .enterTextIntoRegistrationEmailField(email)
+                .enterTextIntoRegistrationPasswordField(password)
+                .checkErrorsMessages(expectedMessages)
         ;
+
+    }
+    // Method to provide parameters for the test. checking different combinations of userName, email, password inside one test
+    public Object[][] parametersForTestValidationMessages() {
+        return new Object[][] {
+                {"tr", "tr1", "tr2", ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"taras", "tr1", "tr2", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD}
+        };
 
     }
 }
