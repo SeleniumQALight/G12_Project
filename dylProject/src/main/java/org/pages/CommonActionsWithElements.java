152f01e4bd920cabe86d253cdb8baf57f8039b8b
Andrew <my_email@test.com>
2025-07-17 21:54:42 +0300

    Using Copilot

--- dylProject/src/main/java/org/pages/CommonActionsWithElements.java
+++ dylProject/src/main/java/org/pages/CommonActionsWithElements.java
@@ -2,8 +2,10 @@ package org.pages;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -171,6 +173,37 @@ logger.error ("Checkbox not found");
             Select select = new Select(webElement);
             select.selectByValue(value);
             logger.info("Value '" + value + "' was selected in dropdown " + getElementName(webElement));
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    //accept Alert
+    protected void acceptAlert() {
+        try {
+            webDriver.switchTo().alert().accept();
+            logger.info("Alert was accepted");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    //scroll to element using Actions
+    protected void scrollToElement(WebElement webElement) {
+        try {
+            Actions actions = new Actions(webDriver);
+            actions.moveToElement(webElement).perform();
+            logger.info("Scrolled to element: " + getElementName(webElement));
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    //open new tab
+    protected void openNewTab() {
+        try {
+            ((JavascriptExecutor) webDriver).executeScript("window.open()");
+            logger.info("New tab was opened");
         } catch (Exception e) {
             printErrorAndStopTest(e);
         }
