8fbecb498a36ff18cd61425f44604c66a09c0b8c
anjelikataneeva <lika.taneeeva@gmail.com>
2025-07-23 09:00:04 +0300

    cw12 commit part1

---
+++ taneevaProj/src/test/java/org/loginTests/LoginTestWithPageObjectWithExcel.java
@@ -0,0 +1,32 @@
+package org.loginTests;
+
+import org.baseTest.BaseTest;
+import org.junit.Test;
+import org.utils.ExcelDriver;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.data.TestData.VALID_LOGIN_UI;
+import static org.data.TestData.VALID_PASSWORD_UI;
+
+public class LoginTestWithPageObjectWithExcel extends BaseTest {
+    @Test
+    public void validLogin() throws IOException {
+        Map<String, String> dataForValidLogin =
+                ExcelDriver.getData(ConfigProvider.configProperties.DATA_FILE(), "validLogOn");
+
+        pageProvider.getLoginPage().
+                openLoginPage()
+                .enterTextIntoInputLogin(dataForValidLogin.get("login"))
+                .enterTextIntoPassword(dataForValidLogin.get("pass"))
+                .clickOnButtonSignIn();
+
+        pageProvider.getHomePage()
+                .checkButtonCreatePostVisible();
+
+        pageProvider.getLoginPage()
+                .checkInputUserNameAndPasswordNotVisible();
+    }
+
+}
