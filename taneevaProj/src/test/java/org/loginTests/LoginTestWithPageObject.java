ba827d1cacc43925d40bbc84012e8cee682e8f55
anjelikataneeva <lika.taneeeva@gmail.com>
2025-07-29 20:00:04 +0300

    suites + rule

--- taneevaProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ taneevaProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,12 +1,15 @@
 package org.loginTests;
 
 import org.baseTest.BaseTest;
+import org.categories.SmokeTestsFilter;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.data.TestData.*;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
+    @Category(SmokeTestsFilter.class)
     public void validLogin() {
         pageProvider.getLoginPage().
                 openLoginPage()
