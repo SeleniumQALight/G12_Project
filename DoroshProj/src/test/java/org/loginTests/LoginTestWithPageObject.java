5428f46c732937d6c21fd79846696cb8196a4887
dora <alinadorosh65@gmail.com>
2025-07-29 21:53:32 +0300

    Allure reports

--- DoroshProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ DoroshProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,5 +1,6 @@
 package org.loginTests;
 
+import io.qameta.allure.*;
 import org.baseTest.BaseTest;
 import org.categories.SmokeTestsFilter;
 import org.junit.Test;
@@ -7,11 +8,19 @@ import org.junit.experimental.categories.Category;
 
 import static org.data.TestData.*;
 
+@Epic("Allure examples")
+@Feature("Junit 4 support")
+
 public class LoginTestWithPageObject extends BaseTest {
     @Test
-@Category(SmokeTestsFilter.class)
+    @Category(SmokeTestsFilter.class)
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Story("Base support for bdd annotations")
     public void validLogin() {
-
         pageProvider.getLoginPage()
                 .openLoginPage()
                 .enterTextIntoInputLogin(VALID_LOGIN_UI)
