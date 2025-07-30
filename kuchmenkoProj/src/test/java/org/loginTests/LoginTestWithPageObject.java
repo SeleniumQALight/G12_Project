d940e160d8efcb5fc18be49c28c7898dbd4d8736
k.kuchmenko <kateryna.kuchmenko@pitchbook.com>
2025-07-30 21:25:02 +0300

    allure

--- kuchmenkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ kuchmenkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,5 +1,11 @@
 package org.loginTests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.Story;
 import org.baseTest.BaseTest;
 import org.categories.SmokeTestsFilter;
 import org.junit.Test;
@@ -8,8 +14,16 @@ import org.junit.experimental.categories.Category;
 import static org.data.TestData.VALID_LOGIN_UI;
 import static org.data.TestData.VALID_PASSWORD_UI;
 
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends BaseTest {
     @Test
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Story("Base support for bdd annotations")
     @Category(SmokeTestsFilter.class)
     public void validLogin() {
         pageProvider.getLoginPage()
