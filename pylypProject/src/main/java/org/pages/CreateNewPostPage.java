74a431151635eae8dbbd8dee796e15d89bab06ab
tradulenko <radulenko@gmail.com>
2025-07-31 15:16:02 +0300

    fix of several projects

--- pylypProject/src/main/java/org/pages/CreateNewPostPage.java
+++ pylypProject/src/main/java/org/pages/CreateNewPostPage.java
@@ -63,6 +63,8 @@ public class CreateNewPostPage extends ParentPage {
 
     public CreateNewPostPage setCheckboxState(String desiredState) {
         setCheckboxState(checkboxIsThisPostUnique, desiredState);
+        return this;
+    }
 
     public CreateNewPostPage selectTextInDropdownAccess(String textForSelectiom) {
         selectTextInDropDown(dropdownAccess, textForSelectiom);
