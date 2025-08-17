8953e8f8bbf7e42ddea4a7e4d366ab7298f6e99c
Andrew <my_email@test.com>
2025-08-17 18:26:18 +0300

    Complete second test

--- dylProject/src/test/java/org/apiTests/PrivatbankApiTests.java
+++ dylProject/src/test/java/org/apiTests/PrivatbankApiTests.java
@@ -17,7 +17,7 @@ public class PrivatbankApiTests {
     private Logger logger = Logger.getLogger(getClass());
 
     @Test
-    public void getArcExchangeRates() {
+    public void getArcExchangeRatesAndCheckMainFields() {
         RestAssured.baseURI = "https://api.privatbank.ua/p24api/";
         List<String> currencies = List.of("AUD", "AZN", "BYN", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP",
                 "GEL", "HUF", "ILS", "JPY", "KZT", "MDL", "NOK", "PLN", "SEK", "SGD",
@@ -55,7 +55,52 @@ public class PrivatbankApiTests {
                     )
                     .isEqualTo(expectedObject);
         softAssertions.assertAll();
-        }
     }
+
+    @Test
+    public void getArcExchangeRatesAndCheckRates() {
+        RestAssured.baseURI = "https://api.privatbank.ua/p24api/";
+        ExchangeRateByDateDto actualResponse = given()
+                .queryParam("date", "22.03.2022")
+                .log().all()
+                .when()
+                .get(EndPointsPrivatBank.EXCHANGE_RATES)
+                .then()
+                .log().all()
+                .extract().body().as(ExchangeRateByDateDto.class);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (ExchangeRateDto rate : actualResponse.getExchangeRate()) {
+            if (rate.getSaleRateNB() != null) {
+                softAssertions
+                        .assertThat(rate.getSaleRateNB())
+                        .as(rate.getCurrency() + " saleRateNB")
+                        .isGreaterThan(0);
+            }
+            if (rate.getPurchaseRateNB() != null) {
+                softAssertions
+                        .assertThat(rate.getPurchaseRateNB())
+                        .as(rate.getCurrency() + " purchaseRateNB")
+                        .isGreaterThan(0);
+            }
+            if (rate.getSaleRate() != null) {
+                softAssertions
+                        .assertThat(rate.getSaleRate())
+                        .as(rate.getCurrency() + " purchaseRate")
+                        .isGreaterThan(0);
+            }
+            if (rate.getPurchaseRate() != null) {
+                softAssertions
+                        .assertThat(rate.getPurchaseRate())
+                        .as(rate.getCurrency() + " saleRate")
+                        .isGreaterThan(0);
+            }
+
+        }
+        logger.info("Exchange rates for all currencies are more than 0");
+        softAssertions.assertAll();
+    }
+}
 
 
