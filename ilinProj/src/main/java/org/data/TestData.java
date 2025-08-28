accc25604c12752e9c6c9076129607d026e7ca93
artemilin <ilinartem293@gmail.com>
2025-08-28 19:24:24 +0300

    lesson 26-08

--- ilinProj/src/main/java/org/data/TestData.java
+++ ilinProj/src/main/java/org/data/TestData.java
@@ -2,9 +2,13 @@ package org.data;
 
 import org.utils.ConfigProvider;
 
+import java.util.Locale;
+
 public class TestData {
     public final static String VALID_LOGIN_UI = System.getProperty("defaultLogin", ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
     public final static String VALID_NEW_LOGIN_UI = "new"+System.getProperty("defaultLogin", ConfigProvider.configHiddenProperties.login());
 
+    public static final String VALID_LOGIN_API = "g12artemuser".toLowerCase();
+    public static final String VALID_PASSWORD_API = "1234567890-=";
 }
