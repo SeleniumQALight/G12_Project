c478956c09fe57d7003a94d5d58e85b2297b217c
anjelikataneeva <lika.taneeeva@gmail.com>
2025-09-01 12:51:11 +0300

    short fix

--- taneevaProj/src/main/java/org/pages/ParentPage.java
+++ taneevaProj/src/main/java/org/pages/ParentPage.java
@@ -12,9 +12,8 @@ import java.util.ArrayList;
 
 abstract class ParentPage extends CommonActionsWithElements {
     static String environment = System.getProperty("env", "aqa");
-//    protected String baseURL = "https://"+environment+"-complexapp.onrender.com";
     protected String baseURL = ConfigProvider.configProperties.base_url().replace("[env]",environment);
-   // protected String baseURL = "https://"+environment+"-complexapp.onrender.com";
+
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
     }
