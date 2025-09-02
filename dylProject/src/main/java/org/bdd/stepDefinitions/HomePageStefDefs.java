2ce19e3e971c42357d6b9bf5bd8bb34bea68ee89
Andrew <my_email@test.com>
2025-09-02 21:50:54 +0300

    Commit 2

--- dylProject/src/main/java/org/bdd/stepDefinitions/HomePageStefDefs.java
+++ dylProject/src/main/java/org/bdd/stepDefinitions/HomePageStefDefs.java
@@ -1,7 +1,9 @@
 package org.bdd.stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
 
 public class HomePageStefDefs extends MainStepDefs {
     public HomePageStefDefs(WebDriverHelper webDriverHelper) {
@@ -12,5 +14,23 @@ public class HomePageStefDefs extends MainStepDefs {
     public void iSeeAvatarOnHomePage() {
         pageProvider.getHomePage().getHeaderForLoggedUserElement()
                 .checkButtonSignOutVisible();
+    }
+
+    @And("I open Home page as {string} user and {string} password")
+    public void iOpenHomePageAsDefaultUserAndDefaultPassword(String userName,String password) {
+        if (ApiStepdefs.DEFAULT.equalsIgnoreCase(userName)) {
+            userName = TestData.VALID_LOGIN_API;
+        }
+        if (ApiStepdefs.DEFAULT.equalsIgnoreCase(password)) {
+            password = TestData.VALID_LOGIN_API;
+        }
+        pageProvider.getLoginPage()
+                .openLoginPage()
+                .enterTextIntoInputLogin(userName)
+                .enterTextIntoPassword(password)
+                .clickOnButtonSignIn();
+
+        pageProvider.getHomePage()
+                .checkIsRedirectToHomePage();
     }
 }
---
+++ dylProject/src/main/java/org/bdd/stepDefinitions/MyProfileStepdefs.java
@@ -0,0 +1,21 @@
+package org.bdd.stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.bdd.helpers.WebDriverHelper;
+
+public class MyProfileStepdefs extends MainStepDefs {
+    public MyProfileStepdefs(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
+    @Then("I was redirected to MyProfile page")
+    public void iWasRedirectedToMyProfilePage() {
+        pageProvider.getMyProfilePage().checkIsRedirectToMyProfilePage();
+    }
+
+    @And("I see {} posts in Posts list on MyProfile Page")
+    public void iSeePostsInPostsListOnMyProfilePage(String numberOfPosts) {
+    pageProvider.getMyProfilePage().checkNumberOfPosts(numberOfPosts);
+    }
+}
