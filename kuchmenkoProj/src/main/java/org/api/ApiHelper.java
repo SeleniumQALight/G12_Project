b1c5cd6ce3db21126c16dd33a00e0ad39315c6a4
k.kuchmenko <kateryna.kuchmenko@pitchbook.com>
2025-09-02 17:53:46 +0300

    CreatePostByApi created

--- kuchmenkoProj/src/main/java/org/api/ApiHelper.java
+++ kuchmenkoProj/src/main/java/org/api/ApiHelper.java
@@ -8,10 +8,18 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
+import org.api.dto.responseDto.PostsDto;
+import org.data.TestData;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
 public class ApiHelper {
+
+    private Logger logger = Logger.getLogger(getClass());
 
     public static RequestSpecification requestSpecification = new RequestSpecBuilder()
             .setContentType(ContentType.JSON)
@@ -38,5 +46,56 @@ public class ApiHelper {
 //                .log().all()
 //                .statusCode(expectedStatusCode);
                 .spec(responseSpecification.statusCode(expectedStatusCode));
+    }
+
+    /**
+     * Method works with default user for API
+     *
+     * @return
+     */
+    public String getToken() {
+        return getToken(TestData.VALID_LOGIN_API, TestData.VALID_PASSWORD_API);
+    }
+
+    public String getToken(String userName, String password) {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("username", userName);
+        requestBody.put("password", password);
+
+        return given()
+                .spec(requestSpecification)
+                .body(requestBody.toMap())
+                .when()
+                .post(EndPoints.LOGIN)
+                .then()
+                .spec(responseSpecification)
+                .extract().response().body().asString().replace("\"", "");
+    }
+
+    public void deleteAllPostsTillPresent(String userName, String actualToken) {
+        PostsDto[] listOfPosts = this.getAllPostsByUserRequest(userName.toLowerCase())
+                .extract().response().body().as(PostsDto[].class);
+
+        for (int i = 0; i < listOfPosts.length; i++) {
+            deletePostById(actualToken, listOfPosts[i].getId());
+            logger.info(
+                    String.format("Post with id %s and title '%s' was deleted"
+                            , listOfPosts[i].getId()
+                            , listOfPosts[i].getTitle())
+            );
+        }
+    }
+
+    private void deletePostById(String actualToken, String id) {
+        HashMap<String, String > bodyRequest = new HashMap<>();
+        bodyRequest.put("token", actualToken);
+
+        given()
+                .spec(requestSpecification)
+                .body(bodyRequest)
+                .when()
+                .delete(EndPoints.DELETE_POST, id)
+                .then()
+                .spec(responseSpecification);
     }
 }
