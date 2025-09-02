fe3e7fe67ac9d7c19f7834b3d76fb7684b14a62d
artemilin <ilinartem293@gmail.com>
2025-09-02 23:21:33 +0300

    сucumber test 3

---
+++ ilinProj/src/main/java/org/bdd/stepDefinitions/ApiStepdefs.java
@@ -0,0 +1,26 @@
+package org.bdd.stepDefinitions;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import org.api.ApiHelper;
+import org.data.TestData;
+
+public class ApiStepdefs {
+    final static String DEFAULT = "default";
+    private ApiHelper apiHelper = new ApiHelper();
+
+    @Given("I create {} new posts via API for {string} user and {string} password")
+    public void iCreateNewPostsViaAPIForDefaultUserAndDefaultPassword(
+            Integer numberOfPosts, String userName, String password, DataTable dataTable) {
+        if (DEFAULT.equalsIgnoreCase(userName)) {
+            userName = TestData.VALID_LOGIN_API;
+        }
+        if (DEFAULT.equalsIgnoreCase(password)) {
+            password = TestData.VALID_PASSWORD_API;
+        }
+        String token = apiHelper.getToken(userName, password);
+        apiHelper.createPosts(numberOfPosts, token, dataTable.asMap());
+
+
+    }
+}
