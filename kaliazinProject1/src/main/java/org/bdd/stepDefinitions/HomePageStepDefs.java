2820d044e5f7320faea788741b261759cb59215a
yan.kaliazin <yan.kaliazin@pitchbook.com>
2025-09-02 21:49:08 +0300

    e2e test

--- kaliazinProject1/src/main/java/org/bdd/stepDefinitions/HomePageStepDefs.java
+++ kaliazinProject1/src/main/java/org/bdd/stepDefinitions/HomePageStepDefs.java
@@ -1,7 +1,11 @@
 package org.bdd.stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
+
+import static org.bdd.stepDefinitions.ApiStepdefs.DEFAULT;
 
 public class HomePageStepDefs extends MainStepDefs{
     public HomePageStepDefs(WebDriverHelper webDriverHelper) {
@@ -11,5 +15,23 @@ public class HomePageStepDefs extends MainStepDefs{
     @Then("I see avatar on HomePage")
     public void iSeeAvatarOnHomePage() {
         pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonSignOutVisible();
+    }
+
+    @And("I open Home page as {string} user and {string} password")
+    public void iOpenHomePageAsDefaultUserAndDefaultPassword(String userName, String password) {
+        if (DEFAULT.equalsIgnoreCase(userName)){
+            userName = TestData.VALID_LOGIN_API;
+        }
+        if (DEFAULT.equalsIgnoreCase(password)){
+            password = TestData.VALID_PASSWORD_API;
+        }
+        pageProvider.getLoginPage()
+                .openLoginPage()
+        .enterTextIntoInputLogin(userName)
+        .enterTextIntoPassword(password)
+        .clickLoginButtonSignIn();
+
+        pageProvider.getHomePage()
+                .checkIsRedirectToHomePage();
     }
 }
--- kaliazinProject1/src/main/java/org/pages/MyProfilePage.java
+++ kaliazinProject1/src/main/java/org/pages/MyProfilePage.java
@@ -17,6 +17,9 @@ public class MyProfilePage extends ParentPage {
     @FindBy (xpath = "//*[text()='Post successfully deleted.']")
     private WebElement successMessageDelete;
 
+    @FindBy(xpath = ".//a[@class='list-group-item list-group-item-action']")
+    private List<WebElement> postsList;
+
     public MyProfilePage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -69,6 +72,12 @@ public class MyProfilePage extends ParentPage {
 
     private MyProfilePage checkIsMessageSuccessDeletePresent() {
         checkIsElementDisplayed(successMessageDelete);
+        return this;
+    }
+
+    public MyProfilePage checkNumberOfPostsInPosts(String numberOfPosts) {
+        Assert.assertEquals("Number of posts", numberOfPosts, postsList.size());
+
         return this;
     }
 }
