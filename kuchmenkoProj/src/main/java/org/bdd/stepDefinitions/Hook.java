e41f2b66ee6693eef6e5fc33ad5665ef66fe6b4a
k.kuchmenko <kateryna.kuchmenko@pitchbook.com>
2025-09-09 18:06:08 +0300

    e2e test

--- kuchmenkoProj/src/main/java/org/bdd/stepDefinitions/Hook.java
+++ kuchmenkoProj/src/main/java/org/bdd/stepDefinitions/Hook.java
@@ -2,22 +2,32 @@ package org.bdd.stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.restassured.RestAssured;
+import org.api.ApiHelper;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
+import org.pages.ParentPage;
 
 public class Hook {
     WebDriverHelper webDriverHelper;
+    ApiHelper apiHelper = new ApiHelper();
 
     public Hook(WebDriverHelper webDriverHelper) {
         this.webDriverHelper = webDriverHelper;
     }
 
-    @Before
+    @Before(order = 10)
     public void setUp(){
-
+        RestAssured.baseURI = ParentPage.baseURL + "/api/";
     }
 
-    @After
+    @After(order = 15)
     public void tearDown(){
         webDriverHelper.quiteDriver();
+    }
+
+    @Before(value = "@deletePostsTillPresentForDefaultUser", order = 50)
+    public void deletePostsForDefaultUserTillPresent(){
+        apiHelper.deleteAllPostsTillPresent(TestData.VALID_LOGIN_API, TestData.VALID_PASSWORD_API);
     }
 }
