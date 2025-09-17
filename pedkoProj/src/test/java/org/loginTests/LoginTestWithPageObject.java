749b95d8c7cb94d913af45ed00558e489cddec33
dariia <dawulikis@gmail.com>
2025-09-17 22:25:55 +0300

    TestDate was added

--- pedkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ pedkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,15 +1,17 @@
 package org.loginTests;
 
+import data.TestData;
 import org.baseTest.BaseTest;
 import org.junit.Test;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void validLogin(){
-        pageProvider.getLoginPage().openLoginPage();
-        pageProvider.getLoginPage().enterTextIntoInputLogin("qaauto");
-        pageProvider.getLoginPage().enterTextIntoPassword("123456qwerty");
-        pageProvider.getLoginPage().clickOnButtonSignIn();
+        pageProvider.getLoginPage()
+                .openLoginPage()
+                .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
+                .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
+                .clickOnButtonSignIn();
 
         pageProvider.getHomePage().checkButtonSignOutVisible();
 
