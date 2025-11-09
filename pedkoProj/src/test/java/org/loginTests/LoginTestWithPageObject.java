fefd45491d71711f679fc7f68b9d35b67b733799
dariia <dawulikis@gmail.com>
2025-11-09 21:27:35 +0200

    Home work 4 edit post + check header element pedko

--- pedkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ pedkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -10,10 +10,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.pages.LoginPage;
 import org.pages.elements.HeaderForLoggedUserElement;
+import java.time.Duration;
 
 @Epic("Allure examples")
 @Feature("Junit 4 support")
-import java.time.Duration;
+
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
@@ -24,66 +25,65 @@ public class LoginTestWithPageObject extends BaseTest {
     @Issue("123")
     @Issue("432")
     @Story("Base support for bdd annotations")
-    public void validLogin(){
     public void validLogin() {
-        pageProvider.getLoginPage()
-                .openLoginPage()
-                .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
-                .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
-                .clickOnButtonSignIn();
+            pageProvider.getLoginPage()
+                    .openLoginPage()
+                    .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
+                    .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
+                    .clickOnButtonSignIn();
 
-        pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonSignOutVisible();
+            pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonSignOutVisible();
+        }
+
+        @Test
+        @Category(SmokeTestsFilter.class)
+        public void invalidLogin () {
+            pageProvider.getLoginPage()
+                    .openLoginPage()
+                    .enterTextIntoInputLogin("wrongUser")
+                    .enterTextIntoPassword("wrongPass")
+                    .clickOnButtonSignIn();
+
+            pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonSignOutNotVisible();
+            pageProvider.getLoginPage().checkButtonSignInVisible();
+            pageProvider.getLoginPage().checkErrorMessage("Invalid username/password.");
+        }
+
+        @Test
+        public void validLoginAdditionalChecks () {
+            pageProvider.getLoginPage()
+                    .openLoginPage()
+                    .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
+                    .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
+                    .clickOnButtonSignIn();
+
+            pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonCreatePostVisible();
+            pageProvider.getLoginPage().checkInputLoginNotVisible();
+            pageProvider.getLoginPage().checkInputPasswordNotVisible();
+        }
+
+        @Test
+        public void testSignOut () {
+            pageProvider.getLoginPage()
+                    .openLoginPage()
+                    .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
+                    .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
+                    .clickOnButtonSignIn();
+
+            pageProvider.getHomePage()
+                    .getHeaderForLoggedUserElement()
+                    .checkHeaderElementsVisible();
+
+            pageProvider.getHomePage()
+                    .getHeaderForLoggedUserElement()
+                    .clickOnButtonSignOut();
+
+            pageProvider.getHomePage()
+                    .getHeaderForLoggedUserElement()
+                    .checkHeaderElementsNotVisible();
+
+            pageProvider.getLoginPage()
+                    .waitForButtonSignInVisible()
+                    .checkButtonSignInVisible();
+        }
     }
-
-    @Test
-    @Category(SmokeTestsFilter.class)
-    public void invalidLogin() {
-        pageProvider.getLoginPage()
-                .openLoginPage()
-                .enterTextIntoInputLogin("wrongUser")
-                .enterTextIntoPassword("wrongPass")
-                .clickOnButtonSignIn();
-
-        pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonSignOutNotVisible();
-        pageProvider.getLoginPage().checkButtonSignInVisible();
-        pageProvider.getLoginPage().checkErrorMessage("Invalid username/password.");
-    }
-
-    @Test
-    public void validLoginAdditionalChecks() {
-        pageProvider.getLoginPage()
-                .openLoginPage()
-                .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
-                .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
-                .clickOnButtonSignIn();
-
-        pageProvider.getHomePage().getHeaderForLoggedUserElement().checkButtonCreatePostVisible();
-        pageProvider.getLoginPage().checkInputLoginNotVisible();
-        pageProvider.getLoginPage().checkInputPasswordNotVisible();
-    }
-
-    @Test
-    public void testSignOut() {
-        pageProvider.getLoginPage()
-                .openLoginPage()
-                .enterTextIntoInputLogin(TestData.VALID_LOGIN_UI)
-                .enterTextIntoPassword(TestData.VALID_PASSWORD_UI)
-                .clickOnButtonSignIn();
-
-        pageProvider.getHomePage()
-                .getHeaderForLoggedUserElement()
-                .checkHeaderElementsVisible();
-
-        pageProvider.getHomePage()
-                .getHeaderForLoggedUserElement()
-                .clickOnButtonSignOut();
-
-        pageProvider.getHomePage()
-                .getHeaderForLoggedUserElement()
-                .checkHeaderElementsNotVisible();
-
-        pageProvider.getLoginPage()
-                .waitForButtonSignInVisible()
-                .checkButtonSignInVisible();
-    }
-}
